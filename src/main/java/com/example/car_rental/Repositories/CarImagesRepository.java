eba3f6fec535e4e1a00e72a241d1aa2935211240
Magdalena Jaśkiewicz <magdalena.jaskiewicz27@gmail.com>
2021-04-12 20:04:17 +0200

    Tidying up

--- src/main/java/com/example/car_rental/Repositories/CarImagesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.car_rental.Repositories;
-
-import com.example.car_rental.Entities.CarImages;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CarImagesRepository extends CrudRepository<CarImages, Long> {
-}
--- src/main/java/com/example/car_rental/Repositories/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.car_rental.Repositories;
-
-import com.example.car_rental.Entities.Car;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CarRepository extends CrudRepository<Car, Long> {
-}
